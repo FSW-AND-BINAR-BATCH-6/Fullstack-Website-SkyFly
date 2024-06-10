bf4d9c14a3ce20dd5e90fcdeb43d8c6e012a61db
Reinandafaris <reinandafaris7@gmail.com>
2024-06-10 11:08:30 +0700

    first init

--- src/app/(authentication)/otp/page.tsx
+++ src/app/(authentication)/otp/page.tsx
@@ -43,7 +43,7 @@ export default function OtpPage() {
 
       <section className="mt-14">
         <div className="mx-auto w-[30rem]">
-          <Link href="/dashboard/register">
+          <Link href="/register">
             <Button size="sm" className="pe-4">
               <ArrowLeftIcon className="w-5 h-5 mr-2" />
               Back
